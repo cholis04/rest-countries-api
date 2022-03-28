9027925d8712c2dacc86956db13fa0bcb680df07
cholis04 <korunkolis04@gmail.com>
2022-03-28 07:58:18 +0700

    Initialize Proect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,45 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { createGlobalStyle } from 'styled-components';
+
+// Font Import
+import '@fontsource/nunito-sans/300.css';
+import '@fontsource/nunito-sans/400.css';
+import '@fontsource/nunito-sans/600.css';
+import '@fontsource/nunito-sans/800.css';
+
+// Global Style
+export const GlobalStyle = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-family: 'Nunito Sans', sans-serif;
+    background-color:hsl(185, 41%, 84%);
+  }
+  img {
+    display: block;
+    max-width: 100%;
+  }
+  a {
+    text-decoration: none;
+  }
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
