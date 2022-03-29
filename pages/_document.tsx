4197c93449f2f391f10a613b2ceddc588ca3d7f3
cholis04 <korunkolis04@gmail.com>
2022-03-29 07:24:52 +0700

    Added Link Preconncet Google Fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,6 +41,12 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800&display=swap"
             rel="stylesheet"
