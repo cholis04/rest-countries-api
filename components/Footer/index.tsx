f518c44e239d474c643e6013d4b4674546875709
cholis04 <korunkolis04@gmail.com>
2022-04-05 13:22:31 +0700

    Added Footer Component

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// Styles
+const FooterStyled = styled.footer`
+  padding: 4rem 0;
+  margin: 0 auto;
+  width: 100%;
+  max-width: ${(props) => props.theme.maxWidth};
+`;
+
+const AttributionDiv = styled.div`
+  font-size: 12px;
+  text-align: center;
+  color: ${(props) => props.theme.textColor};
+`;
+
+const LinkAttribute = styled.a`
+  text-decoration: underline;
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterStyled>
+      <AttributionDiv>
+        Challenge by{' '}
+        <LinkAttribute
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </LinkAttribute>
+        . Coded by{' '}
+        <LinkAttribute
+          href="https://github.com/cholis04"
+          target="_blank"
+          rel="noreferrer"
+        >
+          cholis04
+        </LinkAttribute>
+        .
+      </AttributionDiv>
+    </FooterStyled>
+  );
 };
 
 export default Footer;
--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -2,15 +2,13 @@ import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
 const MainLayout: React.FC = ({ children }) => {
-  const component = (
+  return (
     <>
       <Navbar />
       {children}
       <Footer />
     </>
   );
-
-  return component;
 };
 
 export default MainLayout;
