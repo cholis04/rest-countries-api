6157d6cb0e4662c4701cf81520e44a827a9db5d2
cholis04 <korunkolis04@gmail.com>
2022-04-11 17:31:58 +0700

    Updated Layout Main add Theme Color Bar

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,9 +1,20 @@
+import Head from 'next/head';
+import { useTheme } from 'styled-components';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
 const MainLayout: React.FC = ({ children }) => {
+  const theme = useTheme();
+  console.log(theme);
   return (
     <>
+      <Head>
+        <meta name="theme-color" content={theme.backgroundColor} />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content={theme.backgroundColor}
+        />
+      </Head>
       <Navbar />
       {children}
       <Footer />
